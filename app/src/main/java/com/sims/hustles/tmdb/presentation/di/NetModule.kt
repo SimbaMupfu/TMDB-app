aa3fd72c32c7fedd637119012ffca75492bb1682
simba_m <scmupfururirwa@gmail.com>
2021-04-21 13:30:25 +0200

    create network module that provides retrofit instance and TMDBService instance

---
+++ app/src/main/java/com/sims/hustles/tmdb/presentation/di/NetModule.kt
@@ -0,0 +1,27 @@
+package com.sims.hustles.tmdb.presentation.di
+
+import com.sims.hustles.tmdb.data.api.TMDBService
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetModule(private val baseURL: String) {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseURL)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesTMDBService(retrofit: Retrofit): TMDBService{
+        return retrofit.create(TMDBService::class.java)
+    }
+}
