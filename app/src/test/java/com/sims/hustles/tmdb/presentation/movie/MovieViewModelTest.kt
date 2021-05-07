8cb0be6d3cd60d64512b55297d7fa1b9fa70b9dd
simba_m <scmupfururirwa@gmail.com>
2021-05-08 01:46:04 +0200

    perform tests to get data and update data

--- app/src/test/java/com/sims/hustles/tmdb/presentation/movie/MovieViewModelTest.kt
+++ app/src/test/java/com/sims/hustles/tmdb/presentation/movie/MovieViewModelTest.kt
@@ -2,11 +2,12 @@ package com.sims.hustles.tmdb.presentation.movie
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
 import com.sims.hustles.tmdb.data.models.movies.Movie
 import com.sims.hustles.tmdb.data.repository.movie.FakeMovieRepository
 import com.sims.hustles.tmdb.domain.usecase.GetMoviesUseCase
 import com.sims.hustles.tmdb.domain.usecase.UpdateMoviesUseCase
-import org.junit.Assert.*
+import com.sims.hustles.tmdb.getOrAwaitValue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -34,6 +35,17 @@ class MovieViewModelTest{
         movies.add(Movie(1, "overview1", "path1", "date1", "title1"))
         movies.add(Movie(2, "overview2", "path2", "date2", "title2"))
 
-        val currentMovieList = viewModel.getMovies()
+        val currentMovieList = viewModel.getMovies().getOrAwaitValue()
+        assertThat(currentMovieList).isEqualTo(movies)
+    }
+
+    @Test
+    fun updateMovies_returnUpdatedList(){
+        val movies = mutableListOf<Movie>()
+        movies.add(Movie(3, "overview3", "path3", "date3", "title3"))
+        movies.add(Movie(4, "overview4", "path4", "date4", "title4"))
+
+        val updatedMovieList = viewModel.updateMovies().getOrAwaitValue()
+        assertThat(updatedMovieList).isEqualTo(movies)
     }
 }
