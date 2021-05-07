03cd143de2564ffda22ddefc90223d3dba1a29c9
simba_m <scmupfururirwa@gmail.com>
2021-05-08 01:28:22 +0200

    Create LiveData function utility to help convert Movie LiveData into a normal list

--- app/src/test/java/com/sims/hustles/tmdb/presentation/movie/MovieViewModelTest.kt
+++ app/src/test/java/com/sims/hustles/tmdb/presentation/movie/MovieViewModelTest.kt
@@ -2,9 +2,14 @@ package com.sims.hustles.tmdb.presentation.movie
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.sims.hustles.tmdb.data.models.movies.Movie
+import com.sims.hustles.tmdb.data.repository.movie.FakeMovieRepository
+import com.sims.hustles.tmdb.domain.usecase.GetMoviesUseCase
+import com.sims.hustles.tmdb.domain.usecase.UpdateMoviesUseCase
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -17,6 +22,18 @@ class MovieViewModelTest{
 
     @Before
     fun setUp() {
+        val fakeMovieRepository = FakeMovieRepository()
+        val getMoviesUseCase = GetMoviesUseCase(fakeMovieRepository)
+        val updateMoviesUseCase = UpdateMoviesUseCase(fakeMovieRepository)
+        viewModel = MovieViewModel(getMoviesUseCase, updateMoviesUseCase)
+    }
 
+    @Test
+    fun getMovies_returnCurrentList(){
+        val movies = mutableListOf<Movie>()
+        movies.add(Movie(1, "overview1", "path1", "date1", "title1"))
+        movies.add(Movie(2, "overview2", "path2", "date2", "title2"))
+
+        val currentMovieList = viewModel.getMovies()
     }
 }
